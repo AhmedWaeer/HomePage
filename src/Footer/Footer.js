aa030e3563cc93096cea34c5d9cb2f3bf58b8cc5
AhmedWaeer <78096972+AhmedWaeer@users.noreply.github.com>
2021-07-08 19:26:01 +0200

    Update Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="text-center mx-auto text-black font-bold">
+    <div className="text-center mx-auto text-black font-bold mb-10">
     © 2021,Made with ❤️ by A.ABDO
     </div>
   );
