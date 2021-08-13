61606d91efa8c0fff30f556ba40cb0b7f2b75cf3
Ahmed WAeer <ahmedwaeer@gmail.com>
2021-08-13 02:33:09 +0200

    images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     {
         title: 'A.ABDO Home',
         description: "My own homepage, displaying a collection of my most recent projects. Built with Next.Js (+ React), deployed on Vercel.",
-        image: '/images/home.PNG',
+        image: '/images/home.jpg',
         tags: ['#React', '#Next.js'],
         source: '',
         visit: 'https://github.com/AhmedWaeer/HomePage.git',
@@ -12,7 +12,7 @@ export const projects = [
     {
         title: 'CairoFresh',
         description: "My first particaipation in a live project. I was part of a team developing a live webshop app.Built with React, Gatsby, Express and Sass.",
-        image: '/images/CairoFresh.PNG',
+        image: '/images/CairoFresh.jpg',
         tags: ['#React','#Gatsby' ,'#Express'],
         source: 'https://cairofresh.com/',
         visit: '',
@@ -23,7 +23,7 @@ export const projects = [
     {
         title: 'TechBlog',
         description: "A simple Web app to utilize the full stack: using NodeJS, Express, Handlebars, and deployed on Heroku.",
-        image: '/images/Tech.PNG',
+        image: '/images/Tech.jpg',
         tags: ['#Handlebars','#Express','#MongoDB'],
         source: 'https://tech-blogpost.herokuapp.com/',
         visit: 'https://github.com/AhmedWaeer/TechBlog.git',
@@ -32,7 +32,7 @@ export const projects = [
     {
         title: 'Workout Tracker',
         description: "A full-stack workout tracker deployed on Heroku. Uses Mongoose + MongoDB for persistence.",
-        image: '/images/wrkout.PNG',
+        image: '/images/Workout.jpg',
         tags: ['#Express', '#Node.js','#MongoDB'],
         source: 'https://your-tracker.herokuapp.com/',
         visit: 'https://github.com/AhmedWaeer/Workout-Tracker.git',
@@ -41,7 +41,7 @@ export const projects = [
     {
         title: 'Code Quiz',
         description: "A simple quiz app to test your coding knowledge!. Built with vanilla JS, HTML and CSS (Bootstap).",
-        image: '/images/CodeQuiz.PNG',
+        image: '/images/CodeQuiz.jpg',
         tags: ['#HTML','#CSS' ,'#JavaScript'],
         source: 'https://qodequiz.netlify.app/',
         visit: 'https://github.com/AhmedWaeer/CodeQuiz.git',
@@ -50,7 +50,7 @@ export const projects = [
     {
         title: 'Word Guess Game',
         description: " A weather dashboard built using vanilla JS, HTML and CSS (Bootstrap). Uses the Open Weather API to query for data.",
-        image: '/images/WordGame.PNG',
+        image: '/images/WordGame.jpg',
         tags: ['#HTML','#CSS' ,'#JavaScript'],
         source: 'https://wgg.netlify.app/',
         visit: 'https://github.com/AhmedWaeer/Word-Guessing-Game.git',
@@ -58,35 +58,11 @@ export const projects = [
     },
     {
         title: 'Weather Dashboard',
-<<<<<<< HEAD
         description: "A weather dashboard. Uses the Open Weather API to query for data.",
-=======
-        description: "A weather dashboard built using vanilla JS, HTML and CSS (Bootstrap). Uses the Open Weather API to query for data.",
->>>>>>> a5263d386449c27fc6e9a49930b145d2c8eb98fe
-        image: '/images/weather.PNG',
+        image: '/images/Weather.jpg',
         tags: ['#HTML','#CSS' ,'#JavaScript'],
         source: 'https://weatherdb.netlify.app/',
         visit: 'https://github.com/AhmedWaeer/Weather-Dashboard.git',
-<<<<<<< HEAD
-=======
-        id: 3,
-    },
-    {
-        title: 'Workout Tracker',
-        description: "A full-stack workout tracker deployed on Heroku. Uses Mongoose + MongoDB for persistence.",
-        image: '/images/wrkout.PNG',
-        tags: ['React', 'ChatEngine', 'Firebase'],
-        source: 'https://your-tracker.herokuapp.com/',
-        visit: 'https://github.com/AhmedWaeer/Workout-Tracker.git',
-        id: 4,
-    }, {
-        title: 'A.ABDO Home',
-        description: "My own homepage, displaying a collection of my most recent projects. Built with Next.Js (+ React), deployed on Vercel.",
-        image: '/images/home.PNG',
-        tags: ['React', 'NextJS'],
-        source: 'https://aabdo.me',
-        visit: 'https://github.com/AhmedWaeer/HomePage.git',
->>>>>>> a5263d386449c27fc6e9a49930b145d2c8eb98fe
         id: 5,
     },
   
